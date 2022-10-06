211077279451c86827cb33a3272740b8b298c2a3
John Kosmicke <kozjob@yahoo.com>
2022-10-05 20:53:26 -0700

    first cut rref

--- src/matrix.py
+++ src/matrix.py
@@ -68,6 +68,89 @@ class Matrix:
                    R.elements[i][j] = dot_product(self.row(i), M.col(j)) 
             return R
 
+    def get_pivot_row(self, column_index):
+        for i in range(self.num_rows):
+            bool_left_zeros = True
+            for j in range(column_index):
+                if self.elements[i][j] != 0:
+                    bool_left_zeros = False
+            if bool_left_zeros and self.elements[i][column_index] != 0:
+                return i
+        return None
+
+    def swap_rows(self, row_index1,row_index2):
+        ret_M = self.copy()
+        temp = ret_M.elements[row_index1]
+        ret_M.elements[row_index1] = ret_M.elements[row_index2]
+        ret_M.elements[row_index2] = temp
+        return ret_M
+
+    def normalize_row(self, row_index):
+        ret_M = self.copy()
+        row = ret_M.elements[row_index]
+        scale_factor, elt_index = 0, 0
+        while scale_factor == 0 and elt_index < len(row):
+            scale_factor = row[elt_index]
+            elt_index += 1
+        if scale_factor != 0:
+            for elt_index in range(len(row)):
+                row[elt_index] /= scale_factor
+        ret_M.elements[row_index] = row
+        return ret_M
+
+    def clear_below(self, row_index):
+        ret_M = self.copy()
+        row = ret_M.row(row_index)
+        key_index, i  = -1, 0
+
+        while key_index == -1 and i < len(row):
+            if row[i] != 0:
+                key_index = i
+                break
+            i += 1
+     
+        if key_index != -1:
+            for i in range(row_index + 1, len(ret_M.elements)):
+                multiplier = ret_M.elements[i][key_index] / row[key_index]
+                for j in range(len(ret_M.elements[i])):
+                    ret_M.elements[i][j] -= multiplier * row[j]
+
+        return ret_M
+
+    def clear_above(self, row_index):
+        ret_M = self.copy()
+        row = ret_M.row(row_index)
+        key_index, i  = -1, 0
+
+        while key_index == -1 and i < len(row):
+            if row[i] != 0:
+                key_index = i
+                break
+            i += 1
+        
+        if key_index != -1:
+            for i in range(row_index):
+                multiplier = ret_M.elements[i][key_index] / row[key_index]
+                for j in range(len(ret_M.elements[i])):
+                    ret_M.elements[i][j] -= multiplier * row[j]
+        
+        return ret_M
+
+    def rref(self):
+        ret_M = self.copy()
+        row_index = 0
+        for column_index in range(ret_M.num_cols):
+            pivot_index = ret_M.get_pivot_row(column_index)
+            if not pivot_index == None:
+                if not pivot_index == row_index:
+                    ret_M = ret_M.swap_rows(row_index, pivot_index)
+                ret_M = ret_M.normalize_row(row_index)
+                ret_M = ret_M.clear_above(row_index)
+                ret_M = ret_M.clear_below(row_index)
+                row_index += 1
+        return ret_M
+
+
     def print_row( self, r):
         s = '[' 
         for i in range(len(self.elements[r]) - 1):
